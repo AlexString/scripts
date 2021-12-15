089eb661059872d60f48e2fe4cc1eb2c35ff35e0
AlexString <github.com/Alexstring>
2021-12-15 17:06:19 -0600

    Refactor script

--- python/grep_aliases.py
+++ python/grep_aliases.py
@@ -3,6 +3,15 @@ import re, os, sys
 def colored(r, g, b, text):
     return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
 
+def isPattern(text):
+	return re.search(r'#(.*)[{}]', text)
+
+def printContent(text):
+	if 'alias' not in text:
+		print(line.strip("\n"))
+	else:
+		print(AliasColor + text[6:].strip("\n"))
+
 if __name__ == "__main__":
 	try: # manipulate current directories for executing script
 		current_directory = os.getcwd()
@@ -13,27 +22,27 @@ if __name__ == "__main__":
 		sys.exit(0)
 		
 	with open('.bash_aliases','r') as bash_aliases_file:
-		alias = colored(230,230,0,"alias")
-		flag = False
+		TitleColor = [38,230,0]  			# green
+		AliasColor = colored(230,230,0,"alias") # yellow
+		bracketFlag = False
+		new_line = '\n'
+
 		for line in bash_aliases_file:
-			if line == '\n': # escape empty lines
+			if line is new_line:
 				continue
 
-			# filter content title/end  { }
-			result = re.search(r'#(.*)[{}]', line)
-			if result:
+			if isPattern(line):
 				if '{' in line:
-					flag = True
+					bracketFlag = True
 				elif '}' in line:
-					flag = False
+					bracketFlag = False
 				
-				print(colored(38, 230, 0, line.strip("\n")))
+				print(colored(TitleColor[0], TitleColor[1], TitleColor[2], 
+				line.strip("\n")))
 				continue 
-			# print content
-			if flag is True:
-				if 'alias' not in line:
-					print(line.strip("\n"))
-					continue
-				print(alias + line[6:].strip("\n"))
+
+			if bracketFlag is True:
+				printContent(line)
+
 	# go back to directory
 	os.chdir(current_directory)
